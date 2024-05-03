853cb632fb1851694b2bcffc82dac632abbf01df
Hussain Ali <huali235@gmail.com>
2024-05-03 18:45:11 -0400

    creating, loading web components

---
+++ components/MenuPage.js
@@ -0,0 +1,11 @@
+export class MenuPage extends HTMLElement {
+  constructor() {
+    super();
+  }
+  connectedCallback() {
+    const template = document.getElementById("menu-page-template");
+    const content = template.content.cloneNode(true);
+    this.appendChild(content);
+  }
+}
+customElements.define("menu-page", MenuPage);
--- services/Router.js
+++ services/Router.js
@@ -22,17 +22,14 @@ const Router = {
 
     switch (route) {
       case "/":
-        pageElement = document.createElement("h1");
-        pageElement.textContent = "Menu";
+        pageElement = document.createElement("menu-page");
         break;
       case "/order":
-        pageElement = document.createElement("h1");
-        pageElement.textContent = "Order";
+        pageElement = document.createElement("order-page");
         break;
       default:
         if (route.startsWith("/product-")) {
-          pageElement = document.createElement("h1");
-          pageElement.textContent = "Product";
+          pageElement = document.createElement("details-page");
           const paramId = route.substring(route.lastIndexOf("-") + 1);
           pageElement.dataset.id = paramId;
         }
