8c0c73021b271f88ad1193cfca71358559a308a0
Hussain Ali <huali235@gmail.com>
2024-05-06 17:43:06 -0400

    rendering menu

--- components/MenuPage.js
+++ components/MenuPage.js
@@ -22,6 +22,32 @@ export class MenuPage extends HTMLElement {
     const template = document.getElementById("menu-page-template");
     const content = template.content.cloneNode(true);
     this.root.appendChild(content);
+
+    window.addEventListener("appmenuchange", () => {
+      this.render();
+    });
+  }
+
+  render() {
+    if (app.store.menu) {
+      for (let category of app.store.menu) {
+        const liCategory = document.createElement("li");
+        liCategory.innerHTML = `
+          <h3>${category.name}</h3>
+          <ul class='category'>
+          </ul>
+        `;
+        this.root.querySelector("#menu").appendChild(liCategory);
+
+        category.products.forEach((product) => {
+          const item = document.createElement("product-item");
+          item.dataset.product = JSON.stringify(product);
+          liCategory.querySelector("ul").appendChild(item);
+        });
+      }
+    } else {
+      this.root.querySelector("#menu").innerHTML = "Loading....";
+    }
   }
 }
 customElements.define("menu-page", MenuPage);
